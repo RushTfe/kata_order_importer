481d94c1c0f34d2edb775bbd0051413008193695
pgalindo <rushtfe@gmail.com>
2024-01-20 23:45:58 +0000

    Segunda aproximación a importación de datos, con datos dummy desde el caso de uso.
    - En esta aproximación se hace todo en una sola iteración sobre orders.

--- src/main/java/com/pgalindo/kata/order/importer/service/impl/OrderServiceImpl.java
+++ src/main/java/com/pgalindo/kata/order/importer/service/impl/OrderServiceImpl.java
@@ -9,6 +9,7 @@ import com.pgalindo.kata.order.importer.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -40,73 +41,49 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public void saveAll(List<OrderInput> orders) {
-
-        OrderSaveHelper orderSaveHelper = new OrderSaveHelper();
+        List<Order> orderEntities = new ArrayList<>();
 
         Map<String, Priority> priorityMap = new HashMap<>();
         Map<String, SalesChannel> salesChannelMap = new HashMap<>();
         Map<String, ItemType> itemTypeMap = new HashMap<>();
         Map<String, Region> regionMap = new HashMap<>();
+        Map<String, CountryHelper> countryHelperMap = new HashMap<>();
         Map<String, Country> countryMap = new HashMap<>();
 
-        orders.forEach(order -> addToOrderRelationsSaveHelper(order, orderSaveHelper));
+        for (OrderInput order : orders) {
+            Priority priority = priorityMap.computeIfAbsent(order.priority(), priorityService::findPriorityOrCreate);
 
-        orderSaveHelper.getPriorities()
-                .forEach(priorityName ->
-                        priorityMap.put(priorityName, priorityService.findPriorityOrCreate(priorityName))
-                );
+            SalesChannel salesChannel = salesChannelMap.computeIfAbsent(order.salesChannel(), salesChannelService::findSalesChannelOrCreate);
 
-        orderSaveHelper.getSaleChannels()
-                .forEach(salesChannelName ->
-                        salesChannelMap.put(salesChannelName, salesChannelService.findSalesChannelOrCreate(salesChannelName))
-                );
+            ItemType itemType = itemTypeMap.computeIfAbsent(order.itemType(), itemTypeService::findItemTypesOrCreate);
 
-        orderSaveHelper.getItemTypes()
-                .forEach(itemTypeName ->
-                        itemTypeMap.put(itemTypeName, itemTypeService.findItemTypesOrCreate(itemTypeName))
-                );
+            Region region = regionMap.computeIfAbsent(order.region(), regionService::findPriorityOrCreate);
 
-        orderSaveHelper.getRegions()
-                .forEach(regionName ->
-                        regionMap.put(regionName, regionService.findPriorityOrCreate(regionName))
-                );
+            Country country = countryMap.computeIfAbsent(
+                    order.country(),
+                    countryName ->countryService.findCountryOrCreate(countryName, regionMap.get(order.region()))
+            );
 
-        orderSaveHelper.getCountries()
-                .forEach(countryHelper ->
-                        countryMap.put(
-                                countryHelper.getCountryName(),
-                                countryService.findCountryOrCreate(
-                                        countryHelper.getCountryName(),
-                                        regionMap.get(countryHelper.getRegionName())
-                                )
-                        )
-                );
 
-        List<Order> orderEntities = orders.stream()
-                .map(order -> new Order(order.uuid(),
-                        countryMap.get(order.country()),
-                        itemTypeMap.get(order.itemType()),
-                        salesChannelMap.get(order.salesChannel()),
-                        priorityMap.get(order.priority()),
-                        order.date(),
-                        order.shipDate(),
-                        order.unitsSold(),
-                        order.unitPrice(),
-                        order.unitCost(),
-                        order.totalRevenue(),
-                        order.totalCost(),
-                        order.totalProfit()))
-                .toList();
+            Order orderEntity = new Order(
+                    order.uuid(),
+                    country,
+                    itemType,
+                    salesChannel,
+                    priority,
+                    order.date(),
+                    order.shipDate(),
+                    order.unitsSold(),
+                    order.unitPrice(),
+                    order.unitCost(),
+                    order.totalRevenue(),
+                    order.totalCost(),
+                    order.totalProfit()
+            );
+
+            orderEntities.add(orderEntity);
+        }
 
         orderRepository.saveAll(orderEntities);
-
-    }
-
-    private void addToOrderRelationsSaveHelper(OrderInput order, OrderSaveHelper orderSaveHelper) {
-        orderSaveHelper.getItemTypes().add(order.itemType());
-        orderSaveHelper.getPriorities().add(order.priority());
-        orderSaveHelper.getRegions().add(order.region());
-        orderSaveHelper.getSaleChannels().add(order.salesChannel());
-        orderSaveHelper.getCountries().add(new CountryHelper(order.country(), order.region()));
     }
 }
