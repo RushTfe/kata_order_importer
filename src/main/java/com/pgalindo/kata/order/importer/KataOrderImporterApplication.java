245f031b2fb9b346d79b7770eba08fd07bc57fb6
pgalindo <rushtfe@gmail.com>
2024-01-20 20:01:03 +0000

    Added swagger configuration

--- src/main/java/com/pgalindo/kata/order/importer/KataOrderImporterApplication.java
+++ src/main/java/com/pgalindo/kata/order/importer/KataOrderImporterApplication.java
@@ -3,7 +3,7 @@ package com.pgalindo.kata.order.importer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.pgalindo.kata.order.importer"}, scanBasePackageClasses = KataOrderImporterApplication.class)
 public class KataOrderImporterApplication {
 
 	public static void main(String[] args) {
