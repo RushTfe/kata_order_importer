92912f4b84fbcc87723e26cae38a5d1ca539d958
pgalindo <rushtfe@gmail.com>
2024-01-20 16:54:40 +0000

    Primer commit básico

---
+++ src/test/java/com/pgalindo/kata/order/importer/KataOrderImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgalindo.kata.order.importer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataOrderImporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
