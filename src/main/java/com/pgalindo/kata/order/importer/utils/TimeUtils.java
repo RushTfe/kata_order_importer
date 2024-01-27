3a2afc72d627db2c98dabce40fa7bba78b53b742
RushTfe <pedrogalindotrabajo@hotmail.com>
2024-01-27 22:06:12 +0000

    Generación de CSV
    - Formateo de fecha para CSV

--- src/main/java/com/pgalindo/kata/order/importer/utils/TimeUtils.java
+++ src/main/java/com/pgalindo/kata/order/importer/utils/TimeUtils.java
@@ -5,13 +5,26 @@ import java.time.format.DateTimeFormatter;
 
 public class TimeUtils {
 
+    private static final String CSV_FORMAT = "dd/mm/yyyy";
+    private static final String CLIENT_FORMAT = "M/d/yyyy";
+
     public static LocalDate clientStringToLocalDate(String date) {
         if (date == null) {
             return null;
         }
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yyyy");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(CLIENT_FORMAT);
         return LocalDate.parse(date, formatter);
+    }
+
+    public static String localDateToCsvStringFormatted(LocalDate localDate) {
+        if (localDate == null) {
+            return null;
+        }
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(CSV_FORMAT);
+
+        return localDate.format(formatter);
     }
 
     public static float elapsedMillisToSeconds(long millisAfter, long millisBefore) {
