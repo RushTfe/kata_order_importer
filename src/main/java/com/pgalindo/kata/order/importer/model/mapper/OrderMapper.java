ab11e69849875b65ceb7283c36de7f8a3c9b43a6
RushTfe <pedrogalindotrabajo@hotmail.com>
2024-01-28 13:19:17 +0000

    Generación de CSV
    - Mapper para pasar de la projection a response

--- src/main/java/com/pgalindo/kata/order/importer/model/mapper/OrderMapper.java
+++ src/main/java/com/pgalindo/kata/order/importer/model/mapper/OrderMapper.java
@@ -2,9 +2,12 @@ package com.pgalindo.kata.order.importer.model.mapper;
 
 import com.pgalindo.kata.order.importer.model.entity.*;
 import com.pgalindo.kata.order.importer.model.service.OrderInput;
+import com.pgalindo.kata.order.importer.usecase.get.orders.response.OrderSummaryFieldResponse;
+import com.pgalindo.kata.order.importer.utils.NumberUtils;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
 
 @Mapper(componentModel = "spring")
 public interface OrderMapper {
@@ -20,5 +23,17 @@ public interface OrderMapper {
                             SalesChannel salesChannel,
                             ItemType itemType,
                             Country country);
+
+    @Mapping(expression = "java(this.bigDecimalToString(projection.getTotalCost()))", target = "totalCost")
+    @Mapping(expression = "java(this.bigDecimalToString(projection.getTotalProfit()))", target = "totalProfit")
+    @Mapping(expression = "java(this.integerToString(projection.getTotalCount()))", target = "count")
+    OrderSummaryFieldResponse projectionToOrderSummaryFieldResponse(SummaryProjection projection);
+
+    default String bigDecimalToString(BigDecimal number) {
+        return NumberUtils.bigDecimalToUiString(number);
+    }
+    default String integerToString(Integer number) {
+        return NumberUtils.integerToUiString(number);
+    }
 
 }
--- src/main/java/com/pgalindo/kata/order/importer/usecase/get/orders/OrdersUseCase.java
+++ src/main/java/com/pgalindo/kata/order/importer/usecase/get/orders/OrdersUseCase.java
@@ -1,14 +1,12 @@
 package com.pgalindo.kata.order.importer.usecase.get.orders;
 
-import com.pgalindo.kata.order.importer.service.CountryService;
+import com.pgalindo.kata.order.importer.model.mapper.OrderMapper;
 import com.pgalindo.kata.order.importer.service.OrderService;
 import com.pgalindo.kata.order.importer.usecase.get.orders.response.OrderSummaryFieldResponse;
 import com.pgalindo.kata.order.importer.usecase.get.orders.response.OrdersResponse;
-import com.pgalindo.kata.order.importer.utils.NumberUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 @Component
@@ -16,16 +14,13 @@ import java.util.List;
 public class OrdersUseCase {
 
     private final OrderService orderService;
+    private final OrderMapper orderMapper;
 
     public OrdersResponse getOrders() {
 
         List<OrderSummaryFieldResponse> countrySummaries = orderService.findCountrySummaries()
                 .stream()
-                .map(projection -> new OrderSummaryFieldResponse(
-                        NumberUtils.integerToUiString(projection.getTotalCount()),
-                        NumberUtils.bigDecimalToUiString(projection.getTotalCost()),
-                        NumberUtils.bigDecimalToUiString(projection.getTotalProfit()),
-                        projection.getName()))
+                .map(orderMapper::projectionToOrderSummaryFieldResponse)
                 .toList();
 
         return new OrdersResponse(null, countrySummaries, null, null, null);
